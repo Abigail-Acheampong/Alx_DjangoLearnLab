565edddacf8ba3d807029098f920923352fb8cc3
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-03-09 12:54:46 +0000

    include a router to handle all crud operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book  # Import from the current app (api)
 from .serializers import BookSerializer
+from rest_framework import viewsets #for crud operations
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for performing CRUD operations on the Book model.
+    """
+    queryset = Book.objects.all()  # Query all books
+    serializer_class = BookSerializer  # Use the BookSerializer
